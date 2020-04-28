1f93fa59f04839c1890d9066cfd7ee720199129b
ohjuntaek <>
2020-04-28 16:40:08 +0900

    init

---
+++ src/main/java/ex/marketboro/dddprac/DddpracApplication.java
@@ -0,0 +1,13 @@
+package ex.marketboro.dddprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddpracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddpracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ex/marketboro/dddprac/DddpracApplicationTests.java
@@ -0,0 +1,13 @@
+package ex.marketboro.dddprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddpracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
